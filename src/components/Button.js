dffee209f082f46a1fd89cd4c44b2d7658fe8602
Deva9754 <deva.sahu2001@gmail.com>
2024-03-07 17:21:00 +0530

    "fix: button css color header icon"

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
  const Button = ({name})=>{
     return(
         <div>
-<button className=" px-5 m-2 bg bg-gray-900  rounded-lg text-white">
+<button className=" px-5 m-2 bg bg-gray-600  rounded-lg text-white">
     {name}
 </button>
         </div>
