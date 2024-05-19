3382e575ec7efbf7c7178716cfb3095e30cf99eb
Deva9754 <deva.sahu2001@gmail.com>
2024-05-20 00:27:06 +0530

    body-error-fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
+// import MainContainer from "./MainContainer";
 import Sidebar from "./Sidebar";
 
-
-const Body = ()=>{
-    return(
-        <div className=" flex " >
-<Sidebar/>
-<Outlet />
-
-        </div>
-    )
-}
+const Body = () => {
+  return (
+    <div className=" ">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+};
 export default Body;
